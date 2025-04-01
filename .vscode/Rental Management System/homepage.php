9ab4201bf1beb2e107ad3d87d887b41f34c0b98e
k-owino <owinokennedy60@gmail.com>
2025-04-02 00:09:34 +0300

    Welcome page

---
+++ .vscode/Rental Management System/homepage.php
@@ -0,0 +1,129 @@
+<?php
+	include_once 'header.php';
+?>
+<style type="text/css">
+	img{
+		width: 100%;
+		height: 400px;
+		padding: 3px 3px 3px 3px;
+	}
+	
+</style>
+<!-----------------------------HOME----------------------------------->
+        <section id="home">
+        <div class="container">
+            <div class="col-md-8">
+                <div class="welcome">
+                <div class="animation">
+                <h2>Welcome To <span style="color: green;">Eureka Homes Management</span></h2>
+                <p><i>a click to your dream</i></p>
+                </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        <!--------------------------SERVICES------------------------------------->
+        <section id="services">
+        <div class="container">
+            <div class="text-center">
+                <h1>Our Sevices</h1>
+            </div>
+        <div class="row services">
+            <div class="col-md-4 text-center">
+                 <div class="icon">
+                    <li class="fa fa-desktop"></li>
+                </div>
+                <h5>Web Development</h5>
+                <p>We do develop web applicatons</p>
+            </div>
+            <div class="col-md-4 text-center">
+                 <div class="icon">
+                    <li class="fa fa-mobile"></li>
+                </div>
+                <h5>Mobile App Development</h5>
+                <p>We do develop web applicatons</p>
+            </div>
+             <div class="col-md-4 text-center">
+                <div class="icon">
+                    <li class="fa fa-database"></li>
+                </div>
+                <h5>Database Management</h5>
+                <p>We do develop web applicatons</p>
+            </div>
+        </div>
+        </div>
+        </section>
+        <!-------------------------------ABOUT------------------------------------->
+        <section id="about">
+        <div class="container">
+                    <h1>About Us</h1>
+        <div class="row">
+                <div class="col-md-4">
+                    <p><b>Graphic Kenya</b> is an upcoming start-up with the aim of contributing in the technology and creating employment to the young talented developers locally and globally in the tech industry.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                <p>Adobe Photoshop</p>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 80%;">80%</div>
+                </div>
+                <p>Adobe InDesign</p>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 85%;">85%</div>
+                </div>
+                <p>Adobe Illustrator</p>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 81%;">81%</div>
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+<!-------------------------------------CONTACT--------------------------------------->
+    <section id="contact">
+        <div class="container bg-success">
+            <div class="page-header">
+                <h1>Contact Us</h1>
+            </div>
+                <div class="row">
+                <div class="col-md-6">
+                    <form class="form-group">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Phone Number">
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" row="4" placeholder="Type Your Message Here">
+                            </textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">SEND MESSAGE</button>
+                    </form>
+                </div>
+                <div class="col-md-3">
+                    <div class="list-group-item">
+                        <i class="fa fa-address-book"></i><b>Address:</b>Digo Road, Mombasa
+                    </div>
+                    <div class="list-group-item">
+                        <i class="fa fa-mail"></i><b>Email:</b>graphicke@gmail.com
+                    </div>
+                    <div class="list-group-item">
+                        <i class="fa fa-phone"></i><b>Tel:</b>+254 7369 70192
+                    </div>
+                    <div class="list-group-item">
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+	include_once 'footer.php';
+?>
